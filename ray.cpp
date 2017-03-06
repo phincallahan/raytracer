bffa36cb329ddd79c1e611c8333e1e7782028c08
phincallahan <phincallahan14@gmail.com>
2017-03-05 22:15:22 -0600

    Added reflections and super sampling

--- ray.cpp
+++ ray.cpp
@@ -16,8 +16,16 @@ class vec3
         vec3(double xCo, double yCo, double zCo) : 
             x(xCo), y(yCo), z(zCo) { }
 
+        vec3(double v) : x(v), y(v), z(v) { }
+
         vec3 operator+(const vec3 &other) const {
             return vec3(other.x + this->x, other.y + this->y, other.z + this->z);
+        }
+
+        void operator+=(const vec3 &other) {
+            this->x += other.x;
+            this->y += other.y;
+            this->z += other.z;
         }
 
         vec3 operator-(const vec3 &other) const {
@@ -30,6 +38,10 @@ class vec3
 
         vec3 operator*(const double scale) const {
             return vec3(this->x * scale, this->y * scale, this->z * scale);
+        }
+
+        vec3 operator/(const double scale) const {
+            return vec3(this->x / scale, this->y / scale, this->z / scale);
         }
 
         vec3 operator*(const vec3 &other) const {
@@ -126,6 +138,8 @@ class Ray
 class Shape 
 {
     public:
+        explicit Shape(double kr_) : kr(kr_) {}
+        double kr;
         virtual bool intersect(Ray, double *) const = 0;
         virtual vec3 normal(const vec3) const = 0;
         virtual vec3 colorAt(const vec3) const = 0;
@@ -136,7 +150,8 @@ class Sphere : public Shape {
     public:
         vec3 center, color;
         double radius;
-        Sphere(vec3 pos, vec3 col, double r) : center(pos), color(col), radius(r) { }
+        Sphere(vec3 pos, vec3 col, double r, double kr_) : 
+            center(pos), color(col), radius(r), Shape(kr_) { }
 
         //https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
         bool intersect(Ray ray, double *t) const {
@@ -230,7 +245,7 @@ vec3 localLighting(const vec3 &intersect, const vec3 &normal,
         if(normal.dot(lightDir) < 0) 
             continue;
 
-        Ray shadowRay (intersect + lightDir * .000001, lightDir);
+        Ray shadowRay (intersect + lightDir * .0001, lightDir);
         findIntersect(shadowRay, shapes, &t);
 
         if (t >= 0) 
@@ -252,19 +267,29 @@ vec3 localLighting(const vec3 &intersect, const vec3 &normal,
     return vec3(intensity, intensity, intensity);
 }
 
+#define MAX_RAY_DEPTH 8
+
 vec3 trace(const Ray &ray, const vec3 &cameraPos, 
            vector<Shape *> &shapes, 
-           vector<Light *> &lights) {
+           vector<Light *> &lights, int depth) {
+
+    if(depth >= MAX_RAY_DEPTH) return vec3(0.0);
+
     double t;
     Shape * closest_shape = findIntersect(ray, shapes, &t);
     if(t < 0) {
-        return vec3(0.0, 0.0, 0.0);
+        return vec3(0.0);
     } else {
         vec3 intersect = ray.getPoint(t);
         vec3 normal = closest_shape->normal(intersect);
+        vec3 reflDir = normal * 2 * normal.dot(-ray.dir) + ray.dir;
+        reflDir.normalize();
+
+        Ray reflRay (intersect + reflDir * .001, reflDir);
+        vec3 reflColor = trace(reflRay, cameraPos, shapes, lights, depth + 1);
 
         vec3 local = localLighting(intersect, normal, cameraPos, shapes, lights);
-        vec3 color = closest_shape->colorAt(intersect) * local;
+        vec3 color = closest_shape->colorAt(intersect) * local + reflColor * closest_shape->kr;
         return color;
     }
 }
@@ -273,16 +298,23 @@ int main() {
     cimg_library::CImg<double> img(WIDTH, HEIGHT, 1, 3);
     img.fill(0.0);
 
-    Sphere sphere1(vec3(0.0, 0.0, -2.5), vec3(1.0, 0.0, 0.0), 1.0);
-    Sphere sphere2(vec3(-0.25, 0.0, -1.0), vec3(0.0, 1.0, 0.0), .25);
+    Sphere sphere1(vec3(0.0, 0.0, -8.0), vec3(0.6, 0.3, 0.3), .25, 1.0);
+    Sphere sphere2(vec3(-1.0, 0.0, -7.0), vec3(0.0, 1.0, 0.0), .25, 0.0);
+    Sphere sphere3(vec3(1.0, 1.0, -8.5), vec3(0.5, 0.5, 1.0), .25, 0.3);
+    Sphere sphere4(vec3(.75, -.75, -6.5), vec3(0.8, 0.2, 1.0), .25, 0.8);
 
-    vector<Shape *> shapes(2);
+    vector<Shape *> shapes(4);
     shapes[0] = &sphere1;
     shapes[1] = &sphere2;
+    shapes[2] = &sphere3;
+    shapes[3] = &sphere4;
 
-    Light light(vec3(-1.0, 1.0, 0.0), vec3(1.0, 1.0, 1.0));
-    vector<Light *> lights(1);
-    lights[0] = &light;
+    Light light1(vec3(2.0, 6.0, 0.0), vec3(1.0));
+    Light light2(vec3(-2.0, -18.0, 2.0), vec3(0.8));
+
+    vector<Light *> lights(2);
+    lights[0] = &light1;
+    lights[1] = &light2;
 
     // TODO: ADD IN CODE THAT PRODUCES 4x4 FROM ROTATION
     // TODO: CREATE A 3x3 MATRIX CLASS
@@ -291,19 +323,34 @@ int main() {
     vec3 cameraPos = vec3(0.0, 0.0, 0.0);
     Matrix44 cameraMatrix = Matrix44::identity();
 
-    double scale = tan(M_PI / 3);
+    double scale = tan(M_PI / 15);
 
     //Generate the initial rays. One for each pixel in the screen.
     for(int i = 0; i < WIDTH; i++) {
         for(int j = 0; j < HEIGHT; j++) {
-            double x = (2 * (i + 0.5) / (double) WIDTH - 1) *  scale; 
-            double y = (1 - 2 * (j + 0.5) / (double) HEIGHT) * scale; 
 
-            vec3 dir = cameraMatrix.multiply(vec3(x, y, -1), 1.0);
-            dir.normalize();
-            Ray ray (vec3(0.0, 0.0, 0.0), dir);
+            // TODO: PUT ALL OF THIS IN A SAMPLE FUNCTION
+            vec3 c (0.0);
+            double GRID_SIZE = 3;
+            for(int k = 0; k < GRID_SIZE; k++) {
+                for(int l = 0; l < GRID_SIZE; l++) {
+                    double y_off = (0.5/GRID_SIZE) + (l/GRID_SIZE);
+                    double x_off = (0.5/GRID_SIZE) + (k/GRID_SIZE);
 
-            vec3 c = trace(ray, cameraPos, shapes, lights);
+                    double x = (2 * (i + y_off) / (double) WIDTH - 1) *  scale; 
+                    double y = (1 - 2 * (j + x_off) / (double) HEIGHT) * scale; 
+
+                    vec3 dir = cameraMatrix.multiply(vec3(x, y, -1), 1.0);
+                    dir.normalize();
+                    Ray ray (vec3(0.0, 0.0, 0.0), dir);
+
+                    vec3 col = trace(ray, cameraPos, shapes, lights, 0);
+                    c += col;
+                }
+            }
+
+            c = c / (double)(GRID_SIZE * GRID_SIZE);
+
             double colors[3] = { c.x, c.y, c.z };
             img.draw_point(i, j, colors);
         }
