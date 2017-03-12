07f847b0849e3175f14d78b810aa3bdf3dbdd425
phincallahan <phincallahan14@gmail.com>
2017-03-12 15:37:51 -0500

    added a compile script

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+OBJECTS=(ray.cpp matrix33.cpp vec3.cpp camera.cpp)
+INCLUDE=/usr/X11R6/include
+LIB=/usr/X11R6/lib
+FLAGS=(lm lpthread lX11 std=c++11)
+
+clang++ main.cpp ${OBJECTS[*]} -I $INCLUDE -L $LIB ${FLAGS[@]/#/-} 
